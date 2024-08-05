d4680f579dbd2ec7258b8a53258e1c267aea0970
Tri <trimpham98@gmail.com>
2024-08-04 22:58:36 -0700

    Expand list to first 151 pokemon

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -5,7 +5,7 @@ function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
       .then((response) => response.json())
       .then((data) => setPokemon(data.results))
       .catch((error) => console.error("Error fetching Pokémon:", error));

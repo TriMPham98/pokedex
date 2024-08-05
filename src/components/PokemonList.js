67666a58fb40ab992000018cf60fb4adaf39a9e7
Tri <trimpham98@gmail.com>
2024-08-04 23:34:06 -0700

    Display pokemon number and type in main grid

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,12 +2,34 @@ import React, { useState, useEffect } from "react";
 import "./PokemonList.css";
 import PokemonDetail from "./PokemonDetail";
 
+// Define type colors
+const typeColors = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
+
 function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
   const [selectedPokemon, setSelectedPokemon] = useState(null);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1024")
       .then((response) => response.json())
       .then((data) => {
         const fetchDetailedData = data.results.map((p) =>
@@ -42,7 +64,20 @@ function PokemonList() {
               alt={p.name}
               className="pokemon-image"
             />
+            <span className="pokemon-number">
+              #{p.id.toString().padStart(3, "0")}
+            </span>
             <span className="pokemon-name">{p.name}</span>
+            <div className="pokemon-types">
+              {p.types.map((type) => (
+                <span
+                  key={type.type.name}
+                  className="pokemon-type"
+                  style={{ backgroundColor: typeColors[type.type.name] }}>
+                  {type.type.name}
+                </span>
+              ))}
+            </div>
           </div>
         ))}
       </div>

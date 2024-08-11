a93ff2e6cfbc4e38f29891b2e380595aa4646268
Tri <trimpham98@gmail.com>
2024-08-11 15:16:32 -0700

    Remove h1 tag

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Pokédex</h1>
+      {/* <h1>Pokédex</h1> */}
       <PokemonList />
       <ScrollToTop />
     </div>
